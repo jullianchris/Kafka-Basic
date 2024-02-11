719d32b5d7e7c40deaba2c799ef522ef206f778a
jullianchris <jullianchristopher6@gmail.com>
2024-02-11 23:10:36 +0530

    implement in .net api application

---
+++ Sensor.Publisher/Controllers/SensorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sensor.Publisher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SensorController : ControllerBase
+    {
+        private readonly IWeatherDataPublisher weatherDataPublisher;
+
+        public SensorController(IWeatherDataPublisher weatherDataPublisher)
+        {
+            this.weatherDataPublisher = weatherDataPublisher;
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] Weather weather)
+        {
+            await weatherDataPublisher.ProduceAsync(weather);
+        }
+    }
+}
