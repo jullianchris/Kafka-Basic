64d5a983c78e4afe95d76d268ef11ccc866e112d
jullianchris <jullianchristopher6@gmail.com>
2024-02-11 18:54:21 +0530

    basic implementation kafka

---
+++ WeatherConsumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+var config = new ConsumerConfig
+{
+    GroupId          = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset  = AutoOffsetReset.Earliest
+};
+
+using var consumer = new ConsumerBuilder<Null,string>(config).Build();
+consumer.Subscribe("weather-topic");
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if(response != null)
+        {
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather.state}, " +
+                $"Temp: {weather.temparature}F");
+        }
+    }
+}
+catch (Exception)
+{
+
+	throw;
+}
+
+
+public record Weather(string state, int temparature);
